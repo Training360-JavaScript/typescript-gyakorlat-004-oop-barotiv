c3a7e78ded1e360138a8cfede05d6fb0ef99e05a
barotiv <baroti.viktor@gmail.com>
2022-01-06 17:00:49 +0100

    Fulfilled

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,29 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: any[] = [
+    {
+        id: 1,
+        name: 'Superman',
+        sex: 'male',
+        age: 30,
+        health: 10
+    },
+    {
+        id: 2,
+        name: 'Magneto',
+        sex: 'male',
+        age: 40,
+        health: 9
+    },
+    {
+        id: 3,
+        name: 'Suzy',
+        sex: 'female',
+        age: 20,
+        health: 10
+    }
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +39,26 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} humans
  */
-export const transformers: any[] = [];
+export const transformers: any[] = [
+    {
+        id: 4,
+        name: 'Eagle',
+        wings: 2,
+        wheels: 0,
+        clan: 'Birds'
+    },
+    {
+        id: 5,
+        name: 'Ferrari',
+        wings: 0,
+        wheels: 4,
+        clan: 'Cars'
+    },
+    {
+        id: 6,
+        name: 'TU154',
+        wings: 2,
+        wheels: 12,
+        clan: 'Planes'
+    }
+];
